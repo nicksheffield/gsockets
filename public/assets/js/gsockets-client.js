9629536e4a4ee117789eb527df0cc1a6b3fbd824
Nick Sheffield <numbereft@gmail.com>
2014-11-04 18:15:40 +1300

    Missed one of the renames

--- public/assets/js/gsockets-client.js
+++ public/assets/js/gsockets-client.js
@@ -18,27 +18,27 @@ var gsockets = {
 		this.socket.on(event, callback);
 	},
 	announce: function(listen, data){
-		this.socket.emit('gsocket_announce', {listen: listen, data: data})
+		this.socket.emit('gsockets_announce', {listen: listen, data: data})
 	},
 	bounce: function(listen, data){
-		this.socket.emit('gsocket_bounce', {listen: listen, data: data})
+		this.socket.emit('gsockets_bounce', {listen: listen, data: data})
 	},
 	send: function(socketID, listen, data){
-		this.socket.emit('gsocket_send', {listen: listen, data: data, socketID: socketID})
+		this.socket.emit('gsockets_send', {listen: listen, data: data, socketID: socketID})
 	},
 	broadcast: function(listen, data){
-		this.socket.emit('gsocket_broadcast', {listen: listen, data: data})
+		this.socket.emit('gsockets_broadcast', {listen: listen, data: data})
 	},
 	roomAnnounce: function(roomName, listen, data){
-		this.socket.emit('gsocket_roomAnnounce', {listen: listen, data: data, roomName: roomName})
+		this.socket.emit('gsockets_roomAnnounce', {listen: listen, data: data, roomName: roomName})
 	},
 	roomBroadcast: function(roomName, listen, data){
-		this.socket.emit('gsocket_roomBroadcast', {listen: listen, data: data, roomName: roomName})
+		this.socket.emit('gsockets_roomBroadcast', {listen: listen, data: data, roomName: roomName})
 	},
 	join: function(roomName, listen){
-		this.socket.emit('gsocket_join', {listen: listen, roomName: roomName})
+		this.socket.emit('gsockets_join', {listen: listen, roomName: roomName})
 	},
 	leave: function(roomName, listen){
-		this.socket.emit('gsocket_leave', {listen: listen, roomName: roomName})
+		this.socket.emit('gsockets_leave', {listen: listen, roomName: roomName})
 	}
 }
